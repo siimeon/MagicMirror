dff3e82e08a4ca00e6b49995acfa3a81c6228853
Simo Haakana <simo.haakana@eficode.com>
2017-08-25 00:34:43 +0300

    Initial working version

---
+++ public/magic-mirror.js
@@ -0,0 +1,38 @@
+const template = document.createElement('template');
+template.innerHTML = `
+  <style>
+    :host{
+      color: red;
+    }
+    video {
+      background: #222;
+      margin: 0 0 20px 0;
+      width: 100%;
+    }
+  </style>
+`;
+
+class MagicMirror extends HTMLElement {
+  constructor() {
+    super();
+    this.appendChild(template.content.cloneNode(true));
+    this.setupCamera();
+    console.log( window.screen.height* 2);
+    console.log( window.screen.width);
+  }
+
+  setupCamera() {
+    const video =  document.createElement('video');
+    if (navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+        video.src = window.URL.createObjectURL(stream);
+        video.play();
+        video.onloadedmetadata = (e) => {video.play();};
+        this.appendChild(video);
+      });
+    }
+  }
+
+}
+
+customElements.define('magic-mirror', MagicMirror);
